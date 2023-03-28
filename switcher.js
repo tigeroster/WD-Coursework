cef59bbc41efdc0b0fd3a74c3a4019a68d1314f6
tigeroster <lochana8723@gmail.com>
2023-03-28 20:47:40 +0530

    28th March

--- switcher.js
+++ switcher.js
@@ -2,11 +2,28 @@ document.querySelector(".switcher-button").onclick = () => {
   document.querySelector(".switcher").classList.toggle("active");
 };
 
-let themebuttons = document.querySelector('.theme-buttons')
+function changeBackground(color) {
+  document.body.style.background = color;
+  const navbar = document.querySelector('#header');
+  let links = document.querySelectorAll('a.links');
+  if(color == '#2C3E50 '){
+    navbar.style.background = color;
+    document.querySelector(".logo").src="Assets/logo-s.png";    
+    [].slice.call(links).forEach(function(elem) {
+      elem.style.color = 'white';
+  });
 
-themebuttons.forEach(color => {
-    color.addEventListener('click', () => {
-        let dataColor = color.getAttribute('data-color');
-        document.querySelector(':root').setProperty('--main-color', dataColor)
-    })
-})
+  }else if(color == 'white'){
+    navbar.style.background = color;
+    document.querySelector(".logo").src="Assets/logo.png";    
+    [].slice.call(links).forEach(function(elem) {
+      elem.style.color = 'black';
+  });
+  }else if(color == '#212F3D'){
+    navbar.style.background = color;
+    document.querySelector(".logo").src="Assets/logo-s.png";    
+    [].slice.call(links).forEach(function(elem) {
+      elem.style.color = 'white';
+  });
+  }
+}
