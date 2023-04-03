80a9b0b8aa14e17f699b453576deefd5fd6b1a5e
tigeroster <lochana8723@gmail.com>
2023-04-03 22:04:18 +0530

    pre final

--- Student 4/switcher.js
+++ Student 4/switcher.js
@@ -8,20 +8,20 @@ function changeBackground(color) {
   let links = document.querySelectorAll('a.links');
   if(color == '#2C3E50 '){
     navbar.style.background = color;
-    document.querySelector(".logo").src="Assets/logo-s.png";    
+    document.querySelector(".logo").src="/Assets/logo-s.png";    
     [].slice.call(links).forEach(function(elem) {
       elem.style.color = 'white';
   });
 
   }else if(color == 'white'){
     navbar.style.background = color;
-    document.querySelector(".logo").src="Assets/logo.png";    
+    document.querySelector(".logo").src="/Assets/logo.png";    
     [].slice.call(links).forEach(function(elem) {
       elem.style.color = 'black';
   });
   }else if(color == '#212F3D'){
     navbar.style.background = color;
-    document.querySelector(".logo").src="Assets/logo-s.png";    
+    document.querySelector(".logo").src="/Assets/logo-s.png";    
     [].slice.call(links).forEach(function(elem) {
       elem.style.color = 'white';
   });
